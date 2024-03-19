32c1d7b2e7997067200928711cd3bdf4a772563c
Mahesh <kmahesh18001@gmail.com>
2024-03-20 00:07:08 +0530

    files added

---
+++ src/Components/Header.js
@@ -0,0 +1,36 @@
+import react from "react";
+
+export default function Header() {
+  return (
+    <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Add to Cart
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+            </ul>
+          </div>
+          <i className="fa-solid fa-cart-shopping cart"></i>
+        </div>
+      </nav>
+    </div>
+  );
+}
