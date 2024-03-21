f4808cbdb8264de8c899301c12c02489ecd2932b
Mahesh <kmahesh18001@gmail.com>
2024-03-21 11:33:19 +0530

    files added

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -2,29 +2,41 @@ import React from "react";
 
 export default function Cart(props) {
   return (
-    <>
-      {
-        {/* <div className="container d-flex justify-content-center py-4"> */}
-          props.state.length ?
-          <>
-            <div className="cart" style={{ width: "50rem" }}>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Photo</th>
-                    <th>Restuarant</th>
-                    <th>Total</th>
-                  </tr>
-                </thead>
-              </table>
-            </div>
-            <div>
-              <h3> Your cart is empty</h3>
-              <img src="cart.gif" className="cart-gif" />
-            </div>
-          </>
-        {/* </div> */}
-      }
-    </>
+    <section>
+      <div className="container d-flex justify-content-center py-4">
+        {props.data.length ? 
+          <div class="add-to-cart">
+  <table>
+    <thead>
+      <tr>
+        <th>Product</th>
+        <th>Description</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Total</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    {/* <tbody>
+      <tr>
+        <td>Product Name</td>
+        <td>Description of the product goes here.</td>
+        <td>$10.00</td>
+        <td><input type="number" value="1" min="1"/></td>
+        <td>$10.00</td>
+        <td><button>Add to Cart</button></td>
+      </tr>
+    </tbody> */}
+  </table>
+</div>
+
+         : 
+          <div className="d-flex">
+            <h3>Your cart is empty</h3>
+            <img src="cart.gif" alt="Cart" className="cart-gif" />
+          </div>
+        }
+      </div>
+    </section>
   );
 }
